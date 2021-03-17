4cbccf8fe15f7aa82909ca9b8d815f30a06affc6
antonwalker <antonwalker911@gmail.com>
2021-03-16 18:59:37 -0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://fpguyy.herokuapp.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://fpguy.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
